abe01423e5d5a415dba13cca999610831f1669ce
ANNGLORIOUS <mueniannglorious@gmail.com>
2024-07-02 09:19:46 +0300

    initial commit

--- grade.js
+++ grade.js
@@ -1,29 +1,42 @@
-function calculateGrade(mark) {
-    if (mark > 79) {
-      return 'A';
-    } else if (mark >= 60) {
-      return 'B';
-    } else if (mark >= 50) {
-      return 'C';
-    } else if (mark >= 40) {
-      return 'D';
+const readline = require("readline");
+ //This imports the readline module from node.js,helping the user to interact with the commandline after runing the program.
+
+function studentGrader(marks) {
+  //Main function to determine the grade of a student according to their marks.
+
+  if (marks < 0 || marks > 100) {
+    //validates if marks are within the range of 0-100
+    return "Enter marks between 0 and 100.";
+  } else {
+    if (marks > 79) {
+      return "Congratulations, your grade is: A";
+    } else if (marks >= 60) {
+      return "Good trial, your grade is: B";
+    } else if (marks >= 50) {
+      return "Avarage,your grade is: C";
+    } else if (marks >= 40) {
+      return "Need to improve,your grade is: D";
     } else {
-      return 'E';
+      return "See me, your grade is: E";
     }
   }
+}
+// The readline interface prompts the student/teacher to enter their marks through the command line
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-  function studentGradeGenerator() {
-    const mark = parseFloat(('59:'));
+rl.question("Enter your marks: ", (input) => {
+  const marks = parseFloat(input);
 
-    if (isNaN(mark) || mark < 0 || mark > 100) {
-      return 'Invalid input. Please enter a valid mark between 0 and 100.';
-    }
-
-    const grade = calculateGrade(mark);
-    return `The student's grade is: ${grade}`;
+  if (isNaN(marks)) {
+    console.log("Invalid input. grade should be between 0-100"); 
+    //validates if an iput is a number
+  } else {
+    console.log(studentGrader(marks));
   }
-
-
-  const result = studentGradeGenerator();
-  console.log(result);
+  rl.close(); 
+  //closing the readline
+});
   
